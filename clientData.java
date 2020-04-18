7620140a868663d71b7fdb01db0061fc479005ab
aleariza <62802596+aleariza@users.noreply.github.com>
2020-04-17 20:20:26 -0400

    Add files via upload

---
+++ clientData.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ClientData {
+    int    clientID;
+    String firstname;
+    String   lastname;
+    String email;
+    String  password;
+    int gender;
+
+    ProductData (int clientID, String firstname, String lastname, String email, String password) {
+        this.clientID   = clientID;
+        this.firstname  = firstname;
+        this.lastname  = lastname;
+        this.email = emai;
+        this.password = password;
+    }
